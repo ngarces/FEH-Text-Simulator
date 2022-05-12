d2d2f03667a2d2ce2801e605f35e37c1a6804bed
Nicholas Garces <n.garces980825@gmail.com>
2022-05-12 16:34:34 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,217 @@
+const canvasHeight = 1920;
+const canvasWidth = 1080;
+
+var backgroundList;
+var backgroundList2;
+var bgKeyVal1;
+var bgKeyVal2;
+
+$.getJSON("backgrounds.json", function(data) {      
+    bgKeyVal1 = data; 
+    backgroundList = Object.keys(data).sort();
+    var select = document.getElementById("background-select-1")
+    populateSelect(select, backgroundList);
+    select.addEventListener('change', drawBackground);
+});
+
+$.getJSON("dialogue_backgrounds.json", function(data){
+    bgKeyVal2 = data;
+    backgroundList2 = Object.keys(data).sort();
+    var select = document.getElementById("background-select-2")
+    populateSelect(select, backgroundList2);
+    select.addEventListener('change', drawDialogueBackground);
+});
+
+function drawBackground(){
+    var ctx = document.getElementById("background-canvas").getContext("2d");
+    var backgroundInput = document.getElementById("background-input");
+    var backgroundSel = document.getElementById("background-select-1");
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    var backgroundImage = new Image();
+
+    if (backgroundInput.value != ""){
+        backgroundSel.selectedIndex = 0;
+        var file = backgroundInput.files[0];
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = function(e){
+            backgroundImage.src = e.target.result;
+        }
+        backgroundInput.value = null;
+    }
+    else if (backgroundSel.value != ""){
+        backgroundImage.src = "Backgrounds/" + bgKeyVal1[backgroundSel.value];
+    }
+
+    backgroundImage.onload = function(){
+        this.width *= canvasHeight / this.height;
+        this.height = canvasHeight;
+        ctx.drawImage(backgroundImage,  (canvasWidth - this.width) / 2, (canvasHeight - this.height) / 2, this.width, this.height);
+    }
+}
+
+function drawDialogueBackground(){
+    var ctx = document.getElementById("background2-canvas").getContext("2d");
+    var backgroundSel = document.getElementById("background-select-2");
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    var backgroundImage = new Image();
+
+    if (backgroundSel.value != ""){
+        backgroundImage.src = "Dialogue Backgrounds/" + bgKeyVal2[backgroundSel.value];
+    }
+    backgroundImage.onload = function(){
+        this.width = 1625;
+        this.height = 680;
+        ctx.drawImage(backgroundImage, (canvasWidth - this.width) / 2, 234 , this.width, this.height);
+    }
+}
+
+function drawTextBox(){
+    var ctx = document.getElementById("text-box-canvas").getContext("2d");
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    if(document.getElementById("hide-checkbox").checked){
+        return;
+    }
+    var textBox = new Image();
+    textBox.onload = function(){
+        ctx.drawImage(textBox, 0, 1344);
+    }
+    textBox.src = "Text box.png";
+}
+
+function drawNameBox(){
+    var ctx = document.getElementById("name-box-canvas").getContext("2d");
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    if(document.getElementById("hide-checkbox").checked){
+        return;
+    }
+    var nameBox = new Image();
+    nameBox.onload = function(){
+        ctx.drawImage(nameBox, 14, 1142);
+    }
+    nameBox.src = "Name box.png";
+}
+
+function drawText(){
+    var textValue = document.getElementById("text-input").value;
+    var ctx = document.getElementById("text-canvas").getContext("2d");
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    if(document.getElementById("hide-checkbox").checked){
+        return;
+    }
+    ctx.font = "36pt System";
+    ctx.textAlign = "left";
+    ctx.strokeStyle = "rgb(15, 30, 40)";
+    ctx.lineWidth = 8;
+    ctx.fillStyle = "white";
+    var lines = textValue.split("\n");
+    var lineCount = (lines.length > 3) ? 3:lines.length;
+    for (var i = 0; i < lineCount; i++){
+        ctx.strokeText(lines[i], 60, 1525 + (i * 80));
+        ctx.fillText(lines[i], 60, 1525 + (i * 80));
+    }
+
+    //draw name
+    var nameValue = document.getElementById("name-input").value;
+    ctx.font = "39pt System";
+    ctx.textAlign = "center";
+    ctx.strokeStyle = "rgb(50, 30, 10)";
+    ctx.strokeText(nameValue, 311, 1321);
+    ctx.fillText(nameValue, 311, 1321);
+}
+
+function drawPortrait(){
+    var ctx = document.getElementById("portrait-canvas").getContext("2d");
+    var portraitInput = document.getElementById("portrait-input")
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    if (portraitInput.files){
+        var file = portraitInput.files[0];
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = function(e){
+            var image = new Image();
+            image.src = e.target.result;
+            image.onload = function(){
+                this.width *= 1.25;
+                this.height *= 1.25;
+                ctx.drawImage(image, (canvasWidth - this.width) / 2, 0, this.width, this.height);
+            }
+        }
+    }
+}
+
+function saveImage(){
+    var background = document.getElementById("background-canvas");
+    var darken = document.getElementById("darken-canvas");
+    var dialogueBackground = document.getElementById("background2-canvas");
+    var portrait = document.getElementById("portrait-canvas");
+    var textBox = document.getElementById("text-box-canvas");
+    var nameBox = document.getElementById("name-box-canvas");
+    var text = document.getElementById("text-canvas");
+
+    var newCanvas = document.createElement("canvas");
+    var ctx = newCanvas.getContext("2d");
+    newCanvas.width = canvasWidth;
+    newCanvas.height = canvasHeight;
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    [background, darken, dialogueBackground, portrait, textBox, nameBox, text].forEach(function(n){
+        ctx.drawImage(n, 0, 0, canvasWidth, canvasHeight);
+    });
+    var link = document.createElement("a");
+    link.download = "download.png";
+    link.href = newCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    link.click();
+}
+
+function darkenBackground(){
+    var ctx = document.getElementById("darken-canvas").getContext("2d");
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    if (document.getElementById("darken-checkbox").checked){
+        ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    }
+}
+
+function drawAll(){
+    //drawBackground();
+    //drawPortrait();
+    drawTextBox();
+    drawNameBox();
+    drawText();
+}
+
+function populateSelect(select, list){
+    var fragment = document.createDocumentFragment();
+    list.forEach(function(n){
+        var option = document.createElement("option");
+        option.innerHTML = n;
+        option.value = n;
+        fragment.appendChild(option);
+    });
+    select.appendChild(fragment);
+}
+
+window.onload = function(){
+    var nameInput = document.getElementById("name-input");
+    var textInput = document.getElementById("text-input");
+    var portraitInput = document.getElementById("portrait-input");
+    var backgroundInput = document.getElementById("background-input");
+    var backgroundSel = document.getElementById("background-select-2");
+    var saveButton = document.getElementById("save-button");
+    var darkenCheckbox = document.getElementById("darken-checkbox");
+    var hideCheckbox = document.getElementById("hide-checkbox");
+    nameInput.addEventListener('change', drawText);
+    textInput.addEventListener('change', drawText);
+    portraitInput.addEventListener('change', drawPortrait);
+    backgroundInput.addEventListener('change', drawBackground);
+    backgroundSel.addEventListener('change', drawDialogueBackground);
+    saveButton.addEventListener('click', saveImage);
+    darkenCheckbox.addEventListener('change', darkenBackground);
+    hideCheckbox.addEventListener('change', function(){
+        drawTextBox();
+        drawNameBox();
+        drawText();
+    });
+
+    drawAll();
+};
